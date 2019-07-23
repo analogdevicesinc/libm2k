337f501a666280ba1db72f120b794638a58c4853
Daniel Guramulta <daniel.guramulta@analog.com>
2019-07-23 09:18:06 +0300

    Update documentation to d091a94

---
+++ search/classes_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['iio_5fcontext_5fversion',['IIO_CONTEXT_VERSION',['../structlibm2k_1_1_i_i_o___c_o_n_t_e_x_t___v_e_r_s_i_o_n.html',1,'libm2k']]]
+];
---
+++ search/enumvalues_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['channel_5f1',['CHANNEL_1',['../enums_8hpp.html#af39a96895342e305493d2498bed1b7b1a45b70a9415d84742f069301e97ad07a5',1,'libm2k']]],
+  ['channel_5f2',['CHANNEL_2',['../enums_8hpp.html#af39a96895342e305493d2498bed1b7b1a4017e84bc4b3115617ce1cf99bc2156d',1,'libm2k']]]
+];
