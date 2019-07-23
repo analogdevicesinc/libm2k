90250a44e6e555d8e828938b10a5e4ffbb4d6616
Daniel Guramulta <daniel.guramulta@analog.com>
2019-07-23 09:18:06 +0300

    Update documentation to ce0cf95

---
+++ search/variables_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['adc_5fgain_5fch_5f1',['adc_gain_ch_1',['../structlibm2k_1_1_c_a_l_i_b_r_a_t_i_o_n___p_a_r_a_m_e_t_e_r_s.html#af95d5815f94f63824ef3b7fdee3353a5',1,'libm2k::CALIBRATION_PARAMETERS']]],
+  ['adc_5fgain_5fch_5f2',['adc_gain_ch_2',['../structlibm2k_1_1_c_a_l_i_b_r_a_t_i_o_n___p_a_r_a_m_e_t_e_r_s.html#ab39bc944bd18797152284a00e4de22ab',1,'libm2k::CALIBRATION_PARAMETERS']]],
+  ['adc_5foffset_5fch_5f1',['adc_offset_ch_1',['../structlibm2k_1_1_c_a_l_i_b_r_a_t_i_o_n___p_a_r_a_m_e_t_e_r_s.html#abdd022cd2de96e50442c07b20ce4bcbd',1,'libm2k::CALIBRATION_PARAMETERS']]],
+  ['adc_5foffset_5fch_5f2',['adc_offset_ch_2',['../structlibm2k_1_1_c_a_l_i_b_r_a_t_i_o_n___p_a_r_a_m_e_t_e_r_s.html#a839f19b12c4e2012637e813ea48b51f6',1,'libm2k::CALIBRATION_PARAMETERS']]],
+  ['analog_5fcondition',['analog_condition',['../structlibm2k_1_1_s_e_t_t_i_n_g_s.html#a17b390fcf63b213221bc0520abd1b2d8',1,'libm2k::SETTINGS']]]
+];
---
+++ search/variables_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['trigger_5fsource',['trigger_source',['../structlibm2k_1_1_s_e_t_t_i_n_g_s.html#a3df2c31140c22a1fb470f1c9bea77307',1,'libm2k::SETTINGS']]]
+];
