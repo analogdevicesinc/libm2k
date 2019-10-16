aa1268719c402c509252a5f697d205089f52f1e4
Adrian Suciu <adrian.suciu@analog.com>
2019-10-16 18:11:09 +0300

    PythonExamples: Digital add n_bits variable instead of hardcoding nr_of_bits
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- bindings/python/examples/digital.py
+++ bindings/python/examples/digital.py
@@ -1,5 +1,7 @@
 import libm2k
 
+n_bits=4
+
 ctx=libm2k.m2kOpen()
 if ctx is None:
 	print("Connection Error: No ADALM2000 device available/connected to your PC.")
@@ -10,11 +12,11 @@ dig=ctx.getDigital()
 dig.setSampleRateIn(10000)
 dig.setSampleRateOut(10000)
 
-for i in range(3):
+for i in range(n_bits):
     dig.setDirection(i,libm2k.DIO_OUTPUT)
     dig.enableChannel(i,True)
 
-buff=range(8) # create 3 bit binary counter
+buff=range(2**n_bits) # create 3 bit binary counter
 dig.setCyclic(True)
 dig.push(buff)
 
