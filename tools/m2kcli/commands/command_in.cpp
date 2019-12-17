669067b1ff9ce6a36c3cca96566510329ba82375
Teo Perisanu <Teo.Perisanu@analog.com>
2019-12-17 11:44:35 +0200

    m2kcli/command_in: Fix print functions.
    Signed-off-by: Teo Perisanu <Teo.Perisanu@analog.com>

--- tools/m2kcli/commands/command_in.cpp
+++ tools/m2kcli/commands/command_in.cpp
@@ -24,36 +24,73 @@
 
 using namespace libm2k::cli;
 
-void CommandIn::printSamplesCsvFormat(std::vector<uint16_t> &samples)
+void CommandIn::printSamplesCsvFormat(std::vector<uint16_t> &samples, unsigned int nb_samples)
 {
-	for (unsigned short &sample : samples) {
-		std::cout << sample << std::endl;
+	for (unsigned i = 0; i < nb_samples; ++i) {
+		std::cout << samples.at(i) << std::endl;
 	}
 }
 
-void CommandIn::printSamplesCsvFormat(std::vector<std::vector<double>> &samples)
+void CommandIn::printSamplesCsvFormat(std::vector<std::vector<double>> &samples, unsigned int nb_samples,
+				      std::vector<int> &channels)
 {
-	int nbChannels = samples.size() - 1;
-	for (unsigned long i = 0; i < samples[0].size(); i++) {
-		for (int j = 0; j < nbChannels; j++) {
-			std::cout << std::setprecision(16) << samples[j][i] << ',';
+	unsigned int nbChannels = channels.size() - 1;
+	for (unsigned long i = 0; i < nb_samples; i++) {
+		for (unsigned int j = 0; j < nbChannels; j++) {
+			std::cout << std::setprecision(16) << samples[channels[j]][i] << ',';
 		}
-		std::cout << std::setprecision(16) << samples[nbChannels][i] << std::endl;
+		std::cout << std::setprecision(16) << samples[channels[nbChannels]][i] << std::endl;
 	}
 }
 
-void CommandIn::printSamplesBinaryFormat(std::vector<uint16_t> &samples)
+void CommandIn::printRawSamplesBinaryFormat(std::vector<std::vector<double>> &samples, unsigned int nb_samples,
+					    std::vector<int> &channels)
 {
-	char *data = reinterpret_cast<char *>(samples.data());
-	for (unsigned long i = 0; i < samples.size() * 2; ++i) {
+	char *data;
+	if (channels.size() < 2) {
+		std::vector<short> tempSamples(samples[channels[0]].begin(), samples[channels[0]].end());
+		data = reinterpret_cast<char *>(tempSamples.data());
+	} else {
+		std::vector<short> interleavedSamples;
+		for (int i = 0; i < samples[0].size(); ++i) {
+			interleavedSamples.push_back((short) samples[0].at(i));
+			interleavedSamples.push_back((short) samples[1].at(i));
+		}
+		for (auto sample : interleavedSamples) {
+			std::cout << sample << std::endl;
+		}
+		data = reinterpret_cast<char *>(interleavedSamples.data());
+	}
+
+	for (unsigned long i = 0; i < nb_samples * channels.size() * sizeof(short); ++i) {
 		std::cout << data[i];
 	}
 }
 
-void CommandIn::printSamplesBinaryFormat(std::vector<double> &samples)
+void CommandIn::printSamplesBinaryFormat(std::vector<std::vector<double>> &samples, unsigned int nb_samples,
+					 std::vector<int> &channels)
+{
+	char *data;
+	if (channels.size() < 2) {
+		data = reinterpret_cast<char *>(samples[channels[0]].data());
+	} else {
+		std::vector<double> tempSamples;
+		for (int i = 0; i < samples[0].size(); ++i) {
+			tempSamples.push_back(samples[0].at(i));
+			tempSamples.push_back(samples[1].at(i));
+		}
+		data = reinterpret_cast<char *>(tempSamples.data());
+	}
+
+	for (unsigned long i = 0; i < nb_samples * channels.size() * sizeof(double); ++i) {
+		std::cout << data[i];
+	}
+}
+
+void CommandIn::printSamplesBinaryFormat(std::vector<uint16_t> &samples, unsigned int nb_samples)
 {
 	char *data = reinterpret_cast<char *>(samples.data());
-	for (unsigned long i = 0; i < samples.size() * sizeof(double); ++i) {
-		std::cout << std::setprecision(16) << data[i];
+	for (unsigned long i = 0; i < nb_samples * sizeof(uint16_t); ++i) {
+		std::cout << data[i];
 	}
 }
