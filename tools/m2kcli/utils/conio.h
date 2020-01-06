38a7b94130318cd0e9a287db03c35c7ad5a09c68
Teo Perisanu <Teo.Perisanu@analog.com>
2020-01-06 08:34:52 +0200

    tools/m2kcli: Add useful functions for UART terminal.
    The implementation of _getch overrides Windows standard function.
    safe_queue header implements some thread safe functions.
    linux_key_encoder decodes Linux special key codes into Windows key codes.
    
    Signed-off-by: Teo Perisanu <Teo.Perisanu@analog.com>

---
+++ tools/m2kcli/utils/conio.h
@@ -0,0 +1,10 @@
+#if !defined(_WIN32) && !defined(__CYGWIN__)
+
+#ifndef CONIO_H
+#define CONIO_H
+
+int _getch();
+
+#endif //CONIO_H
+
+#endif
---
+++ tools/m2kcli/utils/safe_queue.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Analog Devices, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef LIBM2K_THREAD_SAFE_QUEUE_H
+#define LIBM2K_THREAD_SAFE_QUEUE_H
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template <class T>
+class SafeQueue
+{
+public:
+	SafeQueue() = default;
+
+	~SafeQueue() = default;
+
+	T pop()
+	{
+		std::unique_lock<std::mutex> mlock(locker);
+		while (data.empty())
+		{
+			cond_.wait(mlock);
+		}
+		auto item = data.front();
+		data.pop();
+		return item;
+	}
+
+	void push(const T &item)
+	{
+		std::unique_lock<std::mutex> mlock(locker);
+		data.push(item);
+		mlock.unlock();
+		cond_.notify_one();
+	}
+
+private:
+	std::queue<T> data;
+	std::mutex locker;
+	std::condition_variable cond_;
+};
+
+
+#endif //LIBM2K_THREAD_SAFE_QUEUE_H
