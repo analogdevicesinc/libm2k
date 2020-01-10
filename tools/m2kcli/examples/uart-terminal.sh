59446ece457c82a565b0317e9408c810073d5d1e
Teo Perisanu <Teo.Perisanu@analog.com>
2020-01-10 14:02:17 +0200

    tools/m2kcli: Modify the examples, in order to match to the new structure.
    Signed-off-by: Teo Perisanu <Teo.Perisanu@analog.com>

--- tools/m2kcli/examples/uart-terminal.sh
+++ tools/m2kcli/examples/uart-terminal.sh
@@ -1,6 +1,3 @@
-# example for accessing the terminal of another ADALM-2000
-# GND < --- > GND
-# UART RX (M2K 1) < --- > DIO_7 (M2K 2)
-# UART TX (M2K 1) < --- > DIO_15 (M2K 2)
+# example of continuously communicating with an UART device
 
 m2kcli uart-terminal auto -i baud_rate=115200 rx=15 tx=7 stop_bits=1 parity=none bits_number=8
