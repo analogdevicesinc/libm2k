8f1382587f063c3962d014908c5fead9613cc1f1
Teo Perisanu <Teo.Perisanu@analog.com>
2021-01-18 09:38:22 +0200

    CMakeLists: Fix glog detecting issue.
    Fix the issue for detecting glog when it is installed using apt-get.
    
    Signed-off-by: Teo Perisanu <Teo.Perisanu@analog.com>

--- CI/appveyor/install_glog.bat
+++ CI/appveyor/install_glog.bat
@@ -1,5 +1,6 @@
 set configuration=%~1
 set arch=%~2
+set folder_arch=%~3
 
 if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     set generator=Visual Studio 14 2015
@@ -12,15 +13,14 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
 echo "%generator%"
 echo "%arch%"
 
-set folder_arch=%arch%
 
 if not exist "glog" ( git clone --branch v0.3.5 --depth 1 https://github.com/google/glog )
 pushd glog
 git checkout tags/v0.3.5
-if not exist "build_0_3_5%folder_arch%" ( mkdir build_0_3_5%folder_arch% )
-pushd build_0_3_5%folder_arch%
+if not exist "build_0_3_5-%folder_arch%" ( mkdir build_0_3_5-%folder_arch% )
+pushd build_0_3_5-%folder_arch%
 
-cmake -DWITH_GFLAGS=off -G "%generator%" -A "%arch%" ..
+cmake -DWITH_GFLAGS=off -DBUILD_SHARED_LIBS=on -G "%generator%" -A "%arch%" ..
 cmake --build . --target install --config %configuration%
 
 popd
