79b59d737b5d8944d651095c1de27f2950f0f1c6
Cristina Suteu <cristina.suteu@analog.com>
2021-04-26 20:58:01 +0300

    M2kTests: added digital tests -digital object test -digital channels output & state tests -digital cyclic buffer test -digital trigger conditions test
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

--- tests/digital_functions.py
+++ tests/digital_functions.py
@@ -3,9 +3,36 @@ import numpy as np
 import libm2k
 from multiprocessing.pool import ThreadPool
 import threading
+from pandas import DataFrame
+import random
+import time
+from open_context import ctx
+from scipy.signal import find_peaks
+
+sample = random.randint(0, 255)
+gen_reports = True
+
+
+def result_files(gen_reports):
+    if gen_reports:
+        from create_files import results_file, results_dir, csv, open_files_and_dirs
+        if results_file is None:
+            file, dir_name, csv_path = open_files_and_dirs()
+        else:
+            file = results_file
+            dir_name = results_dir
+            csv_path = csv
+    else:
+        file = []
+        dir_name = []
+        csv_path = []
+
+    return file, dir_name, csv_path
+
+
 def dig_reset(dig):
     """ Reset digital object
-    
+
     Arguments:
         dig  -- Digital object\n
     """
@@ -14,260 +41,252 @@ def dig_reset(dig):
     dig.setCyclic(True)
     dig.setDirection(0b1111111111111111)
     for i in range(16):
-        dig.setOutputMode(i,1)
+        dig.setOutputMode(i, 1)
     dig.enableAllOut(True)
     return
 
+
 def set_digital_trigger(dig):
     """Set the digital trigger
-    
+
     Arguments:
         dig  -- Digital object\n
     """
-    d_trig=dig.getTrigger()
+    d_trig = dig.getTrigger()
     d_trig.setDigitalMode(0)
     d_trig.setDigitalStreamingFlag(True)
     for i in range(16):
-        d_trig.setDigitalCondition(i,5)
+        d_trig.setDigitalCondition(i, 0)
     return d_trig
 
-def ch_0_7_digital_output(dig, buff):
-    """Channels 0 to 7 are set as digital output and channels 8-to 16 are set as digital input.
-    A data buffer is sent to 0-7 and received on 8-16. In ch1 are saved signals for each corresponding channel which will be plotter further
-    
-    Arguments:
-        dig  -- Digital object\n
-        buff  -- Data buffer to be sent\n
-    """
-    #enable 8 output channels
-    for i in range(8):
-        dig.setDirection(i,libm2k.DIO_OUTPUT)
-        dig.enableChannel(i,True)
-    #enable 8 input channels
-    for i in range(8,16):
-        dig.setDirection(i,libm2k.DIO_INPUT)
-        dig.enableChannel(i, True)
-    dig.push(buff)
-    ch1=[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-    data = dig.getSamples(100)
-    for i in range(16):
 
-        if (dig.getDirection(i))==libm2k.DIO_INPUT:
-            for val in data:
-                if(val&(2**i)==2**i):
-                    ch1[i]=np.append(ch1[i],1)
-                else:
-                    ch1[i]=np.append(ch1[i],0)
+def check_digital_channels_state(dig, channel):
+    dig.cancelBufferOut()
+    dig.cancelAcquisition()
+    dig.stopAcquisition()
+    dig.reset()
+    dig_reset(dig)
+    # enable channel under test
+    dig.setDirection(channel, libm2k.DIO_OUTPUT)
+    dig.enableChannel(channel, True)
+    dig.setValueRaw(channel, libm2k.LOW)
+    # enable 8 input channels
+    ch1 = []
+    for level in [libm2k.HIGH, libm2k.LOW]:
+        dig.setValueRaw(channel, level)
+        val = dig.getValueRaw(channel)
+        if val == level:
+            ch1.append(1)
+        else:
+            ch1.append(0)
+
     return ch1
 
-def ch_8_16_digital_output(dig,buff):
-    """Channels 8 to 16 are set as digital output and channels 0-to 7 are set as digital input.
-    A data buffer is sent to 8-16 and received on 0-7. In ch1 are saved signals for each corresponding channel which will be plotter further
-    
-    Arguments:
-        dig  -- Digital object\n
-        buff  -- Data buffer to be sent\n
-    """
-    #enable 8 output channels
-    for i in range(8,16):
-        dig.setDirection(i,libm2k.DIO_OUTPUT)
-        dig.enableChannel(i,True)
-    #enable 8 input channels
-    for i in range(8):
-        dig.setDirection(i,libm2k.DIO_INPUT)
-        dig.enableChannel(i, True)
-    dig.push(buff)
-    ch2=[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-    data = dig.getSamples(100)
+
+def check_digital_output(dig):
+    dig.stopAcquisition()
+    dig.stopBufferOut()
+    dig.reset()
+    d_trig = dig.getTrigger()
+    d_trig.setDigitalMode(0)
+    d_trig.setDigitalStreamingFlag(False)
     for i in range(16):
-        if (dig.getDirection(i))==libm2k.DIO_INPUT:
-            for val in data:
-                #get the signal from the digital channel i
-                if(val&(2**i)==2**i):
-                    ch2[i]=np.append(ch2[i],1)
-                else:
-                    ch2[i]=np.append(ch2[i],0)
+        d_trig.setDigitalCondition(i, libm2k.NO_TRIGGER_DIGITAL)
+    dig.setCyclic(True)
+
+    for i in range(16):
+        dig.setDirection(i, libm2k.DIO_OUTPUT)
+        dig.enableChannel(i, True)
+    ch2 = []
+    for i in range(16):
+        # get the signal from the digital channel i
+        buff = [(2 ** i)] * 16
+        dig.push(buff)
+        print(buff)
+        dig.startAcquisition(16)
+        data = dig.getSamples(16)
+        dig.stopAcquisition()
+        print(data)
+        val = data[0]
+        if val & (2 ** i) == 2 ** i:
+            ch2.append(1)
+        else:
+            ch2.append(0)
     return ch2
 
-def get_data_to_check_trig_condition(d_trig,dig, channel,i,buff):
-    """Set trigger condition and send a buffer. Read and build the signal on the desired channel. The result wil be further used to verify the trigger conditions
-    
-    Arguments:
-        d_trig  -- Digital trigger\n
-        dig  -- Digital object\n
-        channel  --Channel under test\n
-        i  -- Trigger condition\n
-        buff  -- Data buffer \n
-    """
-    ch=[]
-    n=50
-    d_trig.setDigitalCondition(channel,i)
+
+def get_data_to_check_trig_condition(dig, channel, i, buff):
+    ch = []
+    n = 1024
+    delay = 1
+    dig.stopBufferOut()
+    ctx.setTimeout(5000)
+    d_trig = dig.getTrigger()
+    d_trig.reset()
+    d_trig.setDigitalMode(0)
+    d_trig.setDigitalStreamingFlag(False)
+    for j in range(16):
+        d_trig.setDigitalCondition(j, libm2k.NO_TRIGGER_DIGITAL)
+    d_trig.setDigitalCondition(channel, i)
+    d_trig.setDigitalDelay(-delay)
+    dig.setDirection(channel, libm2k.DIO_OUTPUT)
+    dig.enableChannel(channel, True)
+    dig.setCyclic(True)
     dig.push(buff)
     data = dig.getSamples(n)
+    dig.stopAcquisition()
 
     for val in data:
-        #print((val))
-        if(val&(2**channel)==2**channel):
-            ch=np.append(ch,1)
+        if val & (2 ** channel) == 2 ** channel:
+            ch = np.append(ch, 1)
         else:
-            ch=np.append(ch,0)
+            ch = np.append(ch, 0)
+
+    dig.stopAcquisition()
+    dig.stopBufferOut()
 
     return ch
 
-def check_digital_trigger(channel,dig, d_trig):
-    """ Verify the digital trigger for the 'rising edge, falling edge, low level, high level, any edge and no trigger' conditions
-    
-    Arguments:
-        channel  -- Digital channel under test\n
-        dig  -- Digital object\n
-        d_trig  -- Digital trigger\n
-    """
+
+def check_digital_trigger(channel, dig, d_trig):
+    file_name, dir_name, csv_path = result_files(gen_reports)
     dig_reset(dig)
     # low=0
     # high=1
     # level=0.5
-    delay=1
+    delay = 1
 
-    condition=[libm2k.RISING_EDGE,libm2k.FALLING_EDGE,libm2k.LOW_LEVEL,libm2k.HIGH_LEVEL,libm2k.ANY_EDGE,libm2k.NO_TRIGGER]
-    trig_test=[]
-    dig.setDirection(channel,libm2k.DIO_INPUT)
-    dig.enableChannel(channel,True)
-    dig.setDirection(channel+8,libm2k.DIO_OUTPUT)
-    dig.enableChannel(channel+8,True)
-    buff=[512,512,512,0,0,0,0,512,512,512]
-
-    d_trig.setDigitalDelay(-delay)
-
-    #go through all possible trigger conditions
+    condition = [libm2k.RISING_EDGE_DIGITAL, libm2k.FALLING_EDGE_DIGITAL, libm2k.LOW_LEVEL_DIGITAL,
+                 libm2k.HIGH_LEVEL_DIGITAL, libm2k.ANY_EDGE_DIGITAL, libm2k.NO_TRIGGER_DIGITAL]
+    trig_test = []
+    dig.setSampleRateIn(1000000)
+    dig.setSampleRateOut(1000000)
+    # create square buffer containing 2 periods
+    duty = 256 / 2  # 50%
+    signal = np.arange(256) < duty
+    buff = list(map(lambda s: int(s) << channel, signal))
+    buff.extend(buff)
+    # go through all possible trigger conditions
     for i in condition:
-        if i==libm2k.RISING_EDGE:
-            ch=get_data_to_check_trig_condition(d_trig, dig,channel,i, buff)
-            #print(ch)
-            #plt.figure(1)
-            #plt.plot(ch)
-
-            if ch[delay]<=ch[delay+3]:
+        if i == libm2k.RISING_EDGE_DIGITAL:
+            ch = get_data_to_check_trig_condition(dig, channel, i, buff)
+            if ch[delay] <= ch[delay + 3]:
                 print("rising")
-                trig_test=np.append(trig_test,1)
+                trig_test = np.append(trig_test, 1)
             else:
-                trig_test=np.append(trig_test,0)
+                trig_test = np.append(trig_test, 0)
 
-        elif i==libm2k.FALLING_EDGE:
-            ch=get_data_to_check_trig_condition(d_trig, dig,channel,i, buff)
-            # print(ch)
-            # plt.figure(2)
-            # plt.plot(ch)
-            if ch[delay]>=ch[delay+3]:
+            if gen_reports:
+                plot_to_file("Rising Edge Trigger on ch" + str(channel), ch, dir_name,
+                             "digital_trigger_rising_edge_ch" + str(channel) + ".png")
+        elif i == libm2k.FALLING_EDGE_DIGITAL:
+            ch = get_data_to_check_trig_condition(dig, channel, i, buff)
+            if ch[delay] >= ch[delay + 3]:
                 print("falling")
-                trig_test=np.append(trig_test,1)
+                trig_test = np.append(trig_test, 1)
             else:
-                trig_test=np.append(trig_test,0)
-
-        elif i==libm2k.LOW_LEVEL:
-            ch=get_data_to_check_trig_condition(d_trig, dig,channel,i, buff)
-            # print(ch)
-
-            # plt.figure(3)
-            # plt.plot(ch)
-            if ch[delay]==0 :
+                trig_test = np.append(trig_test, 0)
+            if gen_reports:
+                plot_to_file("Falling Edge Trigger on ch" + str(channel), ch, dir_name,
+                             "digital_trigger_falling_edge_ch" + str(channel) + ".png")
+        elif i == libm2k.LOW_LEVEL_DIGITAL:
+            d_trig.reset()
+            ch = get_data_to_check_trig_condition(dig, channel, i, buff)
+            if ch[delay] == 0:
                 print("low")
-                trig_test=np.append(trig_test,1)
+                trig_test = np.append(trig_test, 1)
             else:
-                trig_test=np.append(trig_test,0)
-
-        elif i==libm2k.HIGH_LEVEL:
-            ch=get_data_to_check_trig_condition(d_trig, dig,channel,i, buff)
-            # print(ch)
-            # plt.figure(4)
-            # plt.plot(ch)
-            if ch[delay]==1:
+                trig_test = np.append(trig_test, 0)
+            if gen_reports:
+                plot_to_file("Low Level Trigger on ch" + str(channel), ch, dir_name,
+                             "digital_trigger_low_level_ch" + str(channel) + ".png")
+        elif i == libm2k.HIGH_LEVEL_DIGITAL:
+            d_trig.reset()
+            ch = get_data_to_check_trig_condition(dig, channel, i, buff)
+            if ch[delay] == 1:
                 print("high")
-                trig_test=np.append(trig_test,1)
+                trig_test = np.append(trig_test, 1)
             else:
-                trig_test=np.append(trig_test,0)
+                trig_test = np.append(trig_test, 0)
 
-        elif i==libm2k.ANY_EDGE:
-            ch=get_data_to_check_trig_condition(d_trig, dig,channel,i, buff)
-            # print(ch)
-            # plt.figure(5)
-            # plt.plot(ch)
+            if gen_reports:
+                plot_to_file("High Level Trigger on ch" + str(channel), ch, dir_name,
+                             "digital_trigger_high_level_ch" + str(channel) + ".png")
 
-            if ch[delay]<=ch[delay+3]:
+        elif i == libm2k.ANY_EDGE_DIGITAL:
+            ch = get_data_to_check_trig_condition(dig, channel, i, buff)
+            if ch[delay] <= ch[delay + 3]:
                 print("rising")
-                trig_test=np.append(trig_test,1)
-            elif ch[delay]>=ch[delay+3]:
+                trig_test = np.append(trig_test, 1)
+            elif ch[delay] >= ch[delay + 3]:
                 print("falling")
-                trig_test=np.append(trig_test,1)
+                trig_test = np.append(trig_test, 1)
             else:
-                trig_test=np.append(trig_test,0)
+                trig_test = np.append(trig_test, 0)
 
-        #elif i==5:
+            if gen_reports:
+                plot_to_file("Any Edge Trigger on ch" + str(channel), ch, dir_name,
+                             "digital_trigger_any_edge_ch" + str(channel) + ".png")
 
-    plt.show()
     return trig_test
 
+
 def check_open_drain_mode(dig, channel):
-    """Should verify if the digital channel selected works well in open drain mode
-    
-    Arguments:
-        dig  -- Digital object\n
-        channel  -- Digital Channel under test\n
-    """
-    dig.enableChannel(channel,True)
-    dig.enableChannel(channel+8,True)
+    # TBD
+    dig.enableChannel(channel, True)
+    dig.enableChannel(channel + 8, True)
     dig.setSampleRateIn(1000)
-    dig.setSampleRateOut(1000) # set sample rate and buffer length for a frequency smaller than 450HZ
-    dig.setOutputMode(channel,libm2k.DIO_OPENDRAIN)#OD, PP
-    dig.setDirection(channel,libm2k.DIO_OUTPUT)
-    dig.setDirection(channel+8,libm2k.DIO_INPUT)
-    ch=[]
-    buff=[1,1,1,1,1,0,0,0,0,0,1]#100Hz frequency
+    dig.setSampleRateOut(1000)  # set sample rate and buffer length for a frequency smaller than 450HZ
+    dig.setOutputMode(channel, libm2k.DIO_OPENDRAIN)  # OD, PP
+    dig.setDirection(channel, libm2k.DIO_OUTPUT)
+    dig.setDirection(channel + 8, libm2k.DIO_INPUT)
+    ch = []
+    buff = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1]  # 100Hz frequency
     dig.push(buff)
     data = dig.getSamples(100)
     for val in data:
-            print((val))
-            if(val&(2**channel)==2**channel):
-                ch=np.append(ch,1)
-            else:
-                ch=np.append(ch,0)
-    #dig.setValueRaw(channel,libm2k.HIGH)            
-    #print("raw value of the channel")
-    #print(dig.getValueRaw(channel))
+        print((val))
+        if val & (2 ** channel) == 2 ** channel:
+            ch = np.append(ch, 1)
+        else:
+            ch = np.append(ch, 0)
+    # dig.setValueRaw(channel,libm2k.HIGH)
+    # print("raw value of the channel")
+    # print(dig.getValueRaw(channel))
     # plt.figure(15)
     # plt.plot(ch)
     # plt.show()
 
     return
 
+
 def task1(nb_samples, dig):
     """Parallel process where data is read
-    
+
     Arguments:
         nb_samples  -- Number of samples\n
         dig  -- Digital object\n
     """
     print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
     data = dig.getSamples(nb_samples)
-
-
+    dig.stopAcquisition()
     return data
 
+
 def test_digital_cyclic_buffer(dig, d_trig, channel):
-    """Test for the digital cyclic buffer set to false. 
-    Arguments:
-        dig  -- Digital object\n
-        d_trig  -- Digital trigger\n
-        channel  -- Digital channel under test\n
-    """
-    dig.setDirection(channel,libm2k.DIO_INPUT)
-    dig.enableChannel(channel,True)
-    dig.setDirection(channel+8,libm2k.DIO_OUTPUT)
-    dig.enableChannel(channel+8,True)
-    d_trig.setDigitalCondition(channel,libm2k.LOW_LEVEL)
+    file, dir_name, csv_path = result_files(gen_reports)
+    dig.setDirection(channel, libm2k.DIO_OUTPUT)
+    dig.enableChannel(channel, True)
+    d_trig.setDigitalCondition(channel, libm2k.LOW_LEVEL_DIGITAL)
     dig.setCyclic(True)
-    n=50
-    buff=[512,512,0,0,0,0,512,512]
-    ch=[]
+    dig.setSampleRateOut(1000000)
+    dig.setSampleRateIn(1000000)
+    n = 1024
+    duty = 256 / 2  # 50%
+    signal = np.arange(256) < duty
+    buff = list(map(lambda s: int(s) << channel, signal))
+    buff.extend(buff)
+    ch = []
     pool = ThreadPool(processes=1)
     print("Main thread name: {}".format(threading.main_thread().name))
 
@@ -275,11 +294,57 @@ def test_digital_cyclic_buffer(dig, d_trig, channel):
     dig.push(buff)
     return_val = async_result.get()
     for val in return_val:
-            #print((val))
-        if(val&(2**channel)==2**channel):
-            ch=np.append(ch,1)
+        if val & (2 ** channel) == 2 ** channel:
+            ch = np.append(ch, 1)
         else:
-            ch=np.append(ch,0)
-    plt.plot(ch)
-    plt.show()
-    return ch
+            ch = np.append(ch, 0)
+    dig.stopBufferOut()
+    dig.stopAcquisition()
+    nb_read_buffers = find_peaks(ch)
+    if gen_reports:
+        plot_to_file("Cyclic Buffer on Digital Channel " + str(channel), ch, dir_name,
+                     "cyclic_buff_ch" + str(channel) + ".png")
+        save_data_to_csv(ch, csv_path)
+    if len(nb_read_buffers) >= 3:
+        passed = True
+    else:
+        passed = False
+    return passed
+
+
+def plot_to_file(title, data, dir_name, filename, xlabel=None, ylabel=None, data1=None):
+    """Saves the plots in a separate folder
+
+    Arguments:
+        title  -- Title of the plot\n
+        data  -- Data to be plotted\n
+        filename  -- Name of the file with the plot\n
+
+    Keyword Arguments:
+        xlabel  -- Label of x-Axis (default: {None})
+        ylabel  -- Label of y-Axis(default: {None})
+        data1  --  Data that should be plotted on the same plot(default: {None})
+    """
+    # plot the signals in a separate folder
+    plt.title(title)
+    if xlabel is not None:  # if xlabel and ylabel are not specified there will be default values
+        plt.xlabel(xlabel)
+    else:
+        plt.xlabel('Samples')
+    if ylabel is not None:
+        plt.ylabel(ylabel)
+    else:
+        plt.ylabel('Voltage [V]')
+    plt.grid(b=True)
+    plt.plot(data)  # if a second set of data must be printed (for ch0 and ch1 phase difference in this case)
+    if data1 is not None:
+        plt.plot(data1)
+    plt.savefig(dir_name + "/" + filename)
+    plt.close()
+    return
+
+
+def save_data_to_csv(csv_vals, csv_file):
+    df = DataFrame(csv_vals)
+    df.to_csv(csv_file)
+    return
--- tests/m2k_digital_test.py
+++ tests/m2k_digital_test.py
@@ -1,34 +1,47 @@
 import unittest
 import libm2k
-from digital_functions import dig_reset, set_digital_trigger,ch_0_7_digital_output,ch_8_16_digital_output, check_digital_trigger, check_open_drain_mode
+from digital_functions import dig_reset, set_digital_trigger, check_digital_channels_state, check_digital_output, \
+    check_digital_trigger, check_open_drain_mode
 from digital_functions import test_digital_cyclic_buffer
 from open_context import dig, d_trig
 import logger
-class DigitalTests(unittest.TestCase):
+
+
+class D_DigitalTests(unittest.TestCase):
     """Class where are defined tests for the digital segment
     """
+
     @classmethod
     def setUpClass(self):
         log = logger.myLogger()
         log.info("Digital Segment")
 
-    # #test signal shapes on  ain ch0
-    def test_input_output_digital_channels(self):
-        buf=[255,127,63,31,15,7,3,1,0 ] # to test the channels
-        ch_8_16_input=ch_0_7_digital_output(dig,buf)
-        ch_0_7_input=ch_8_16_digital_output(dig,buf)
-        for i in range(8):
+    def test_digital_objects(self):
+        with self.subTest(msg='test if Digital and Digital Trigger objects were retrieved'):
+            self.assertIsNot((dig, d_trig), (None, None), 'Digital objects: dig, d_trig ')
+
+    def test_state_digital_channels(self):
+        for i in range(16):
+            result = check_digital_channels_state(dig, i)
+            for j in range(len(result)):
+                with self.subTest(i):
+                    self.assertEqual(result[j], 1, "Channel: " + str(i))
+
+    def test_digital_output_channels(self):
+        return_data = check_digital_output(dig)
+        for i in range(16):
             with self.subTest(i):
-                self.assertEqual(all(ch_0_7_input[i]),all(ch_8_16_input[i+8]))
+                self.assertEqual(return_data[i], 1, "Channel: " + str(i))
 
     def test_trig_conditions(self):
-        trig_test=check_digital_trigger(1,dig, d_trig)
-        for i in range(len(trig_test)):
-            with self.subTest(i):
-                self.assertEqual(trig_test[i],1)
+        for i in range(16):
+            trig_test = check_digital_trigger(i, dig, d_trig)
+            for j in range(len(trig_test)):
+                with self.subTest(j):
+                    self.assertEqual(trig_test[j], 1, "Digital channel: "+ str(i))
 
     def test_cyclic_buffer(self):
-        test_digital_cyclic_buffer(dig, d_trig, 1)
-
-
+        for i in range(16):
+            with self.subTest(i):
+                self.assertEqual(test_digital_cyclic_buffer(dig, d_trig, i), 0, "Channel: " + str(i))
 
