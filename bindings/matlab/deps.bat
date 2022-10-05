10aeb46d0b0d50b2c24073fad3f97e9ec7fe9159
Muthi <Ionut.Muthi@analog.com>
2022-10-05 12:04:22 +0300

    updated deps script to run on windows
    Signed-off-by:Ionut Muthi <Ionut.Muthi@analog.com>

--- bindings/matlab/deps.bat
+++ bindings/matlab/deps.bat
@@ -1,33 +1,33 @@
 set root=%cd%
 
-// libiio
-mkdir -p deps
-wget https://github.com/analogdevicesinc/libiio/releases/tag/v0.24/Windows-VS-2022-x64.zip -O libiio-win.zip
-unzip libiio-win.zip
-cp -r libiio*/*.dll deps/
-cp -r libiio*/libiio.exp deps/
-cp -r libiio*/libiio.lib deps/
-mkdir -p deps\include
-cp -r libiio*/iio.h deps/include/
+:: libiio
+mkdir deps
 
+curl.exe  --output  libiio-win.zip --url https://github.com/analogdevicesinc/libiio/releases/download/v0.24/Windows-VS-2022-x64.zip -L
+powershell -Command "Expand-Archive -Path libiio-win.zip -DestinationPath libiio"
+powershell -Command "Get-ChildItem -Path libiio -Recurse -Filter *.dll | Copy-Item -Destination deps "
+powershell -Command "Get-ChildItem -Path libiio -Recurse -Filter libiio.exp | Copy-Item -Destination deps "
+powershell -Command "Get-ChildItem -Path libiio -Recurse -Filter libiio.lib | Copy-Item -Destination deps "
+mkdir deps\include
+powershell -Command "Get-ChildItem -Path libiio -Recurse -Filter iio.h | Copy-Item -Destination deps/include "
 
-// libm2k
+::libm2k
 git clone -b v0.7.0 https://github.com/analogdevicesinc/libm2k.git
 cd libm2k
-mkdir -p build
+mkdir build
 cd build
-cmake -DENABLE_CSHARP=OFF -DENABLE_PYTHON=OFF -DIIO_LIBRARIES:FILEPATH="%root%\deps\libiio.lib" -DIIO_INCLUDE_DIRS:PATH="%root%\deps\include" -DCMAKE_CONFIGURATION_TYPES=RELEASE -G "Visual Studio 17 2022" ..
+cmake -DENABLE_CSHARP=OFF -DENABLE_PYTHON=OFF -DIIO_LIBRARIES:FILEPATH="%root%/deps/libiio.lib" -DIIO_INCLUDE_DIRS:PATH="%root%/deps/include" -DCMAKE_CONFIGURATION_TYPES=RELEASE -G "Visual Studio 17 2022" ..
 
 cmake --build . --config Release
 cd ../..
-cp libm2k/build/libm2k.dll deps/
-cp libm2k/build/libm2k.exp deps/
-cp libm2k/build/libm2k.lib deps/
-cp -r libm2k/include/* deps/include/
+powershell -Command "Get-ChildItem -Path libm2k/build/ -Recurse -Filter libm2k.dll | Copy-Item -Destination deps "
+powershell -Command "Get-ChildItem -Path libm2k/build/ -Recurse -Filter libm2k.exp | Copy-Item -Destination deps "
+powershell -Command "Get-ChildItem -Path libm2k/build/ -Recurse -Filter libm2k.lib | Copy-Item -Destination deps "
+powershell -Command "Get-ChildItem -Path libm2k/include -Recurse  | Copy-Item -Destination deps/include "
 
-// Cleanup
-rm -rf libiio*
-rm -rf libm2k
-rm -rf libiio-win.zip
-mv deps libm2k
+:: Cleanup
+powershell -Command "Remove-Item 'libiio' -Recurse -Force"
+powershell -Command "Remove-Item 'libm2k' -Recurse -Force"
+powershell -Command "Remove-Item 'libiio-win.zip' -Recurse -Force"
+powershell -Command "Rename-Item -Path deps -NewName libm2k"
 
