504e81dfaf8a80770b77e4f42f1d2dc9ccc3a32a
Adrian Stanea <Adrian.Stanea@analog.com>
2024-01-08 12:28:00 +0200

    ci: uncomment azure-pipelines and add fixes for all operating systems.
    Signed-off-by: Adrian Stanea <Adrian.Stanea@analog.com>

--- CI/windows/create_zips.ps1
+++ CI/windows/create_zips.ps1
@@ -12,6 +12,6 @@ $BUILD_DIR = Join-Path $OUTSIDE_BUILD "libm2k-$PLATFORM\*"
 
 $ARTIFACTS_DIR = Get-Item -Path $env:BUILD_ARTIFACTSTAGINGDIRECTORY # path to repo
 New-Item -Path (Join-Path $ARTIFACTS_DIR "libm2k-$PLATFORM") -ItemType Directory
-Copy-Item -Path $BUILD_DIR -Destination (Join-Path $ARTIFACTS_DIR "libm2k-$PLATFORM")
+Copy-Item -Path $BUILD_DIR -Destination (Join-Path $ARTIFACTS_DIR "libm2k-$PLATFORM") -Recurse -Container
 
 Set-Location $SRC_DIR
