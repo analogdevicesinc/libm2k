c29e111f311749c5fb21fd79753f797a9efbfed1
Adrian Stanea <Adrian.Stanea@analog.com>
2024-04-22 12:57:20 +0300

    tests/digital: improve test to handle PG glitches
    - The test now identifies glitches in the PG signal for each DIO channel separately
    as well as for all DIO channels at once.
    
    Signed-off-by: Adrian Stanea <Adrian.Stanea@analog.com>

--- tests/m2k_digital_test.py
+++ tests/m2k_digital_test.py
@@ -51,12 +51,18 @@ class D_DigitalTests(unittest.TestCase):
             msg='Set kernel buffers count on Digital In without raising an error '):
             self.assertEqual(test_err, False, 'Error occured')
 
-    @unittest.skip("This fix is a known bug which was not fixed in firmware v0.32")
+    @unittest.skipIf(ctx.getFirmwareVersion() < 'v0.33', 'Test applicable for firmware v0.33 and later.')
     def test_pattern_generator_pulse(self):
-        # Verifies that the pattern generator does not generate any additional edges. Currently it generates 1 additional edge
-        # before outputting the pattern set. At the end it holds the value of the last sample at the ouput.
+        # Verifies that the pattern generator does not generate any additional edges prior to the pattern set.
+        # At the end it holds the value of the last sample at the output.
         # The measured pattern should be the same as the one set.
+
+        # Single channel
         for i in range(16):
             test_result = test_pattern_generator_pulse(dig, d_trig, i)
             with self.subTest(i):
                 self.assertEqual(test_result, 0, "Found " + str(test_result) + " aditional edges on  Channel: " + str(i))
+        # All channels
+        test_result = test_pattern_generator_pulse(dig, d_trig, -1)
+        with self.subTest(-1):
+            self.assertEqual(test_result, 0, "Found " + str(test_result) + " aditional edges in multi-channel test")
