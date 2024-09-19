89070a40f7b771b82421f460349e8d5625cf67d9
Adrian Stanea <Adrian.Stanea@analog.com>
2024-09-19 16:24:42 +0300

    fix LGPL version missmatch (should be v2.1 not v3)
    Signed-off-by: Adrian Stanea <Adrian.Stanea@analog.com>

--- tools/communication/examples/spi/main.cpp
+++ tools/communication/examples/spi/main.cpp
@@ -1,16 +1,24 @@
 /*
- * Copyright 2019 Analog Devices, Inc.
+ * Copyright (c) 2019 Analog Devices Inc.
+ *
+ * This file is part of libm2k
+ * (see http://www.github.com/analogdevicesinc/libm2k).
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 
 #include <libm2k/m2k.hpp>
 #include <libm2k/contextbuilder.hpp>
