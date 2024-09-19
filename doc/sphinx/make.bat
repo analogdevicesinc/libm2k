980fd50e2b051ba279f450881042ce6692868699
Adrian Stanea <Adrian.Stanea@analog.com>
2024-09-19 15:37:31 +0300

    doc: add missing LGPL v2.1 license header
    Signed-off-by: Adrian Stanea <Adrian.Stanea@analog.com>

--- doc/sphinx/make.bat
+++ doc/sphinx/make.bat
@@ -1,4 +1,24 @@
 @ECHO OFF
+REM
+REM Copyright (c) 2024 Analog Devices Inc.
+REM
+REM This file is part of libm2k
+REM (see http://www.github.com/analogdevicesinc/libm2k).
+REM
+REM This program is free software; you can redistribute it and/or modify
+REM it under the terms of the GNU Lesser General Public License as published by
+REM the Free Software Foundation, either version 2.1 of the License, or
+REM (at your option) any later version.
+REM
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU Lesser General Public License for more details.
+REM
+REM You should have received a copy of the GNU Lesser General Public License
+REM along with this program. If not, see <http://www.gnu.org/licenses/>.
+REM
+@ECHO ON
 
 pushd %~dp0
 
