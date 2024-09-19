4f9fb3cdbbbfc647704af98e8c5aca055a014586
Adrian Stanea <Adrian.Stanea@analog.com>
2024-09-19 15:04:37 +0300

    bindings/python: add missing LGPL 2.1 license header
    Signed-off-by: Adrian Stanea <Adrian.Stanea@analog.com>

--- bindings/python/examples/analogin_sync_2m2ks.py
+++ bindings/python/examples/analogin_sync_2m2ks.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+
 # Requirements: 2x ADALM2000
 #
 # This example assumes the following connections:
