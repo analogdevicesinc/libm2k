ab84c7a18454c6d90bffa18a221e291ed0f11831
Adrian Stanea <Adrian.Stanea@analog.com>
2024-09-19 13:49:56 +0300

    tools/m2kcli/examples: add missing LGPL v2.1 license header
    Signed-off-by: Adrian Stanea <Adrian.Stanea@analog.com>

--- tools/m2kcli/examples/analog_in.sh
+++ tools/m2kcli/examples/analog_in.sh
@@ -1,3 +1,23 @@
+#
+# Copyright (c) 2024 Analog Devices Inc.
+#
+# This file is part of libm2k
+# (see http://www.github.com/analogdevicesinc/libm2k).
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 #capture 2048 processed samples from both channels - csv format
 m2kcli analog-in auto --capture channel=0,1 buffer_size=1024 nb_samples=2048 raw=0
 
